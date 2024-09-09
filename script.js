f202f326592ba6e862913b9c10b1d197e5cb6ddd
ayaaaaaan10 <77223701+ayaaaaaan10@users.noreply.github.com>
2024-09-09 22:42:13 +0530

    Update script.js
    data analyst

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing", {
-        strings: ["Student","Software Engineer"],
+        strings: ["Data Analyst","Software Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student","Software Engineer"],
+        strings: ["Data Analyst","Software Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
